21d1e89636c217a5f9005d11d0f180f2a1712e7b
Raji Mustapha <rajimustapha30@gmail.com>
2022-04-11 21:37:16 +0100

    sihnup fix

--- src/pages/landing/index.js
+++ src/pages/landing/index.js
@@ -3,21 +3,14 @@ import TopNavbar from "components/navbar/top_navbar";
 import BottomNav from "components/navbar/bottom_navbar";
 import Header from "components/header";
 import Market from "components/market";
-import Modal from "components/ModalPopUp";
-import { useState } from "react";
-import SignUpModal from "components/SignUpModal";
 
 const Landing = () => {
-  const [openModal, setOpenModal] = useState(true);
   return (
     <div className={"landing_wrapper"}>
       <TopNavbar />
       <BottomNav />
       <Header />
       <Market />
-      <Modal closeModal={() => setOpenModal(false)} openModal={openModal}>
-        <SignUpModal />
-      </Modal>
     </div>
   );
 };
