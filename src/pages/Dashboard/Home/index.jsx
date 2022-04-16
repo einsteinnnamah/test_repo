27235f59740d317e9bf6f6965278ad3bcc108e29
Raji Mustapha <rajimustapha30@gmail.com>
2022-04-16 15:36:17 +0100

    home

--- src/pages/Dashboard/Home/index.jsx
+++ src/pages/Dashboard/Home/index.jsx
@@ -1,5 +1,19 @@
 const Home = () => {
-  return <div className="pd_home">Home</div>;
+  return (
+    <div className="pd_home">
+      <div className="home_flex">
+        <div className="white_part">
+          <p className="welcome">
+            Hello, <span> Ademola! ✌🏾</span>
+          </p>
+          <p className="sub_welcome">
+            Welcome to ICE Bank. This is an overview of your investments and
+            activities
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
